330783630cd29053ed03f48a882be483de1b674b
Javier <xaviorantes457@gmail.com>
2025-03-03 15:36:33 -0600

    Segundo Commit

---
+++ CadenasdeConsulta6370812/CadenaConsultas.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CadenasdeConsulta6370812
+{
+	public partial class CadenaConsultas : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            // Go to the confirmation page with the encoded URL.
+            Response.Redirect
+            (
+                "CadenasdeConsulta.aspx" +
+                "?ddlCategory=" + ddlCategory.SelectedValue +
+                "&ddlSupplier=" + ddlSupplier.SelectedValue +
+                "&strProduct=" + txtProduct.Text +
+                "&strDescription=" + txtDescription.Text +
+                "&strImage=" + txtImage.Text +
+                "&decPrice=" + txtPrice.Text +
+                "&bytNumberInStock=" + txtNumberInStock.Text +
+                "&bytNumberOnOrder=" + txtNumberOnOrder.Text +
+                "&bytReorderLevel=" + txtReorderLevel.Text
+
+         
+            );
+
+        }
+    }
+}
